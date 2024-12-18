9828df18318f7c42900b86366c60a2833ff7651b
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-18 09:36:00 -0300

    Adiciona métodos para a classe Livro

--- src/Livro.java
+++ src/Livro.java
@@ -4,7 +4,28 @@ public class Livro {
     public String isbn;
     public Boolean disponivel;
 
-    // Metodo exibirDetalhes(): exibir detalhes do livro
-    // Metodo emprestar(): marcar livro como indisponível
-    // Metodo devolver(): marcar livro como disponível
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Disbonível: " + disponivel);
+    }
+
+    public void emprestar() {
+        if (disponivel == true) {
+            disponivel = false;
+            System.out.println("Livro emprestado com sucesso!");
+        } else {
+            System.out.println("O livro está indisponível para emprestar");
+        }
+    }
+
+    public void devolver() {
+        if(disponivel == false) {
+            disponivel = true;
+            System.out.println("Livro devolvido com sucesso!");
+        } else {
+            System.out.println("O livro já está disponível");
+        }
+    }
 }
