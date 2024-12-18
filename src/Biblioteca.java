88fdcc74f98f749c8f2d26d59fa4af23b1b6ec51
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-18 16:12:56 -0300

    Remove comentários auxiliares para criação do algoritmo

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -4,7 +4,7 @@ public class Biblioteca {
    ArrayList<Livro> livros = new ArrayList<>();
    ArrayList<Usuario> usuarios = new ArrayList<>();
 
-//   cadastrarLivro(Livro livro): Adiciona um livro à lista de livros.
+
    public void cadastrarLivro(Livro livro) {
       if(livros.contains(livro) == false) {
          livros.add(livro);
@@ -13,7 +13,7 @@ public class Biblioteca {
          System.out.println("Este livro já está cadastrado");
       }
    }
-//   cadastrarUsuario(Usuario usuario): Adiciona um usuário à lista de usuários.
+
    public void cadastrarUsuario(Usuario usuario) {
       if (usuarios.contains(usuario) == false) {
          usuarios.add(usuario);
@@ -22,8 +22,7 @@ public class Biblioteca {
          System.out.println("Este usuário já está cadastrado");
       }
    }
-//   realizarEmprestimo(String isbn, int idUsuario): Realiza o empréstimo de um livro
-//    para um usuário.
+
    public void realizarEmprestimo(String isbn, int idUsuario) {
       Livro livro = buscarLivroPorIsbn(isbn);
       Usuario usuario = buscarUsuarioPorId(idUsuario);
@@ -47,8 +46,7 @@ public class Biblioteca {
       usuario.adicionarLivro(livro);
       System.out.println("O empréstimo do livro " + livro.titulo + " realizado com sucesso para o usuário " + usuario.nome);
    }
-//   realizarDevolucao(String isbn, int idUsuario): Realiza a devolução de um livro por um
-//    usuário.
+
    public void realizarDevolucao(String isbn, int idUsuario) {
       Livro livro = buscarLivroPorIsbn(isbn);
       Usuario usuario = buscarUsuarioPorId(idUsuario);
@@ -72,7 +70,7 @@ public class Biblioteca {
       usuario.removerLivro(livro);
       System.out.println("A devolução do livro " + livro.titulo + " foi realizada com sucesso pelo usuário " + usuario.nome);
    }
-//  exibirLivrosDisponiveis(): Exibe todos os livros disponíveis para empréstimo.
+
    public void exibirLivrosDisponiveis() {
       System.out.println("Livros disponíveis para empréstimos:");
       for (Livro livro : livros) {
