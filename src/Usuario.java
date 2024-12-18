836e436cf3206436d65f5016027316972780fba7
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-18 09:52:38 -0300

    Adiciona métodos para a classe Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,7 +5,29 @@ public class Usuario {
     public int id;
     ArrayList<Livro> livrosEmprestados = new ArrayList<>();
 
-    // Metodo exibirDetalhes(): Exibir informações do usuário
-    // Metodo adicionarLivro(): Adicionar livro a lista de emprestados
-    // Metodo removerLivro(): Remover livro da lista de emprestados
+    public void exibirDetalhes() {
+        System.out.println("Nome do usuário: " + nome);
+        System.out.println("ID: " + id);
+        for (Livro livro : livrosEmprestados) {
+            System.out.println("-" + livro.titulo);
+        }
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livrosEmprestados.contains(livro) == false) {
+            livrosEmprestados.add(livro);
+            System.out.println("O livro " + livro.titulo + " foi adicionado a lista de emprestados!");
+        } else {
+            System.out.println("Este livro já está na lista de emprestados");
+        }
+    }
+
+    public void removerLivro(Livro livro) {
+        if (livrosEmprestados.contains(livro)) {
+            livrosEmprestados.remove(livro);
+            System.out.println("O livro " + livro.titulo + " foi removido da lista de emprestados!");
+        } else {
+            System.out.println("Este livro ainda não está na lista de emprestados");
+        }
+    }
 }
