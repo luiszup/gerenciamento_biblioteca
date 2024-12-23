8d6a4a31d0d393d688321e71f70a77e22384ec24
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-23 09:18:52 -0300

    Adiciona loop do while e conserta bug na opção 4

--- src/Main.java
+++ src/Main.java
@@ -6,71 +6,74 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int opcao;
 
-        System.out.println("---------Bem-vindo ao sistema de gerenciamento da biblioteca!--------");
-        System.out.println("Escolha uma opção:");
-        System.out.println("1. Cadastrar livro");
-        System.out.println("2. Cadastrar usuário");
-        System.out.println("3. Realizar empréstimo");
-        System.out.println("4. Realizar devolução");
-        System.out.println("5. Exibir livros disponíveis");
-        System.out.println("6. Sair");
-        opcao = input.nextInt();
-        input.nextLine();
+        do {
+            System.out.println("---------Bem-vindo ao sistema de gerenciamento da biblioteca!--------");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Cadastrar livro");
+            System.out.println("2. Cadastrar usuário");
+            System.out.println("3. Realizar empréstimo");
+            System.out.println("4. Realizar devolução");
+            System.out.println("5. Exibir livros disponíveis");
+            System.out.println("6. Sair");
+            opcao = input.nextInt();
+            input.nextLine();
 
-        switch (opcao) {
-            case 1:
-                System.out.println("Digite o título do livro: ");
-                String titulo = input.nextLine();
-                System.out.println("Digite o autor do livro: ");
-                String autor = input.nextLine();
-                System.out.println("Digite o ISBN do livro: ");
-                String isbn = input.nextLine();
-                Livro livro = new Livro();
-                livro.titulo = titulo;
-                livro.autor = autor;
-                livro.isbn = isbn;
-                livro.disponivel = true;
-                biblioteca.cadastrarLivro(livro);
-                break;
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o título do livro: ");
+                    String titulo = input.nextLine();
+                    System.out.println("Digite o autor do livro: ");
+                    String autor = input.nextLine();
+                    System.out.println("Digite o ISBN do livro: ");
+                    String isbn = input.nextLine();
+                    Livro livro = new Livro();
+                    livro.titulo = titulo;
+                    livro.autor = autor;
+                    livro.isbn = isbn;
+                    livro.disponivel = true;
+                    biblioteca.cadastrarLivro(livro);
+                    break;
 
-            case 2:
-                System.out.println("Digite o nome do usuário");
-                String nome = input.nextLine();
-                System.out.println("Digite o ID do usuário: ");
-                int id = input.nextInt();
-                Usuario usuario = new Usuario();
-                usuario.nome = nome;
-                usuario.id = id;
-                biblioteca.cadastrarUsuario(usuario);
-                break;
+                case 2:
+                    System.out.println("Digite o nome do usuário");
+                    String nome = input.nextLine();
+                    System.out.println("Digite o ID do usuário: ");
+                    int id = input.nextInt();
+                    Usuario usuario = new Usuario();
+                    usuario.nome = nome;
+                    usuario.id = id;
+                    biblioteca.cadastrarUsuario(usuario);
+                    break;
 
-            case 3:
-                System.out.println("Digite o ISBN do livro: ");
-                String isbnEmprestimo = input.nextLine();
-                System.out.println("Digite o ID do usuário: ");
-                int idUsuarioEmprestimo = input.nextInt();
-                biblioteca.realizarEmprestimo(isbnEmprestimo, idUsuarioEmprestimo);
-                break;
+                case 3:
+                    System.out.println("Digite o ISBN do livro: ");
+                    String isbnEmprestimo = input.nextLine();
+                    System.out.println("Digite o ID do usuário: ");
+                    int idUsuarioEmprestimo = input.nextInt();
+                    biblioteca.realizarEmprestimo(isbnEmprestimo, idUsuarioEmprestimo);
+                    break;
 
-            case 4:
-                System.out.println("Digite o ISBN do livro: ");
-                String isbnDevolucao = input.nextLine();
-                System.out.println("Digite o ID do usuário: ");
-                int idUsuarioDevolucao = input.nextInt();
-                biblioteca.realizarEmprestimo(isbnDevolucao, idUsuarioDevolucao);
-                break;
+                case 4:
+                    System.out.println("Digite o ISBN do livro: ");
+                    String isbnDevolucao = input.nextLine();
+                    System.out.println("Digite o ID do usuário: ");
+                    int idUsuarioDevolucao = input.nextInt();
+                    biblioteca.realizarDevolucao(isbnDevolucao, idUsuarioDevolucao);
+                    break;
 
-            case 5:
-                biblioteca.exibirLivrosDisponiveis();
-                break;
+                case 5:
+                    biblioteca.exibirLivrosDisponiveis();
+                    break;
 
-            case 6:
-                System.out.println("Até mais!");
-                break;
+                case 6:
+                    System.out.println("Até mais!");
+                    break;
 
-            default:
-                System.out.println("Opção inválida. Digite uma das opções acima");
-        }
+                default:
+                    System.out.println("Opção inválida. Digite uma das opções acima");
+            }
+        } while (opcao != 6);
+
 
         input.close();
     }
